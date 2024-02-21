c410f00204b6b3f41a8799ea057d4a83c56caa20
chauhanRishu <147832011+chauhanRishu@users.noreply.github.com>
2024-02-21 21:09:11 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3 ); // code readability should be high
+
+
+let name="rishu"
+let age=22
+let isLoggedIn=false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => abhi value nhi assign hui h
+// symbol => unique
+
+// object
+
+console.log(typeof "rishu");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof state); // undefined
