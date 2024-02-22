59865af59228a1de56556856f85c60a5440b2782
chauhanRishu <147832011+chauhanRishu@users.noreply.github.com>
2024-02-22 16:20:04 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,31 @@
+/*
+console.log(2>1);
+console.log(1<2);
+console.log(1>=0);
+console.log(2==2);
+console.log(1!=2);
+*/
+
+/*
+console.log("2">1);
+console.log("02">1);
+//avoid to use
+*/
+
+/*
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+//avoid to use
+*/
+
+/*
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+// avoid to use this comparison
+*/
+
+ // ===
+ console.log("2" === 2);
