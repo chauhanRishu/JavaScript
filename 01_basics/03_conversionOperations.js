ee096a490882ab296c482a07cfe645d617f6d855
chauhanRishu <147832011+chauhanRishu@users.noreply.github.com>
2024-02-22 15:25:11 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,77 @@
+let score="33abc"
+let age=null
+let marks=undefined
+let isOpen=true
+let names="Rishu"
+/*
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+console.log("\n**************");
+
+console.log(typeof age);
+console.log(typeof(age));
+let ageInNumber=Number(age)
+console.log(typeof ageInNumber);
+console.log(ageInNumber);
+
+console.log("\n*****************");
+
+console.log(typeof marks);
+console.log(typeof(marks));
+let marksInNumber=Number(marks)
+console.log(typeof marksInNumber);
+console.log(marksInNumber);
+
+console.log("\n***************");
+
+console.log(typeof isOpen);
+console.log(typeof(isOpen));
+let isOpenInNumber=Number(isOpen)
+console.log(typeof isOpenInNumber);
+console.log(isOpenInNumber);
+
+console.log("\n**********************");
+
+console.log(typeof names);
+console.log(typeof(names));
+let namesInNumber=Number(names)
+console.log(typeof namesInNumber);
+console.log(namesInNumber);
+*/
+
+//  "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+/*
+let isLoggedIn=1
+
+let booleanIsLoogedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn);
+*/
+
+/*
+let isLoggedIn=""
+
+let booleanIsLoogedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn);
+*/
+
+let isLoggedIn="rishu"
+
+let booleanIsLoogedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
