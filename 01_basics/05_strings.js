6551469e8113d5dcd3e3712beb0f01cdd88af58f
chauhanRishu <147832011+chauhanRishu@users.noreply.github.com>
2024-02-23 16:04:23 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+const name="rishu"
+const repoCount=50
+
+//console.log(name + repocount + " Value");
+
+console.log('hello my name is ${name} and my repo count is ${repoCount}');
+
+const gameName=new String("rishu-rc")
+/*
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('i'));
+
+const newString=gameName.substring(0,4)
+console.log(newString);
+
+const anotherString=gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne="   rishu   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url="https://rishu.com/rishu%20chauhan"
+console.log(url.replace('%20','-'));
+console.log(url.includes('rishu'));
+*/
+console.log(gameName.split('-'));
