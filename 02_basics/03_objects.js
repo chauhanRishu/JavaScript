12123a81d92dbf39de5ecadd362df11d33f0cc78
chauhanRishu <147832011+chauhanRishu@users.noreply.github.com>
2024-02-24 15:59:06 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,12 @@
+// singleton
+// Object.create
+
+// object literals
+const JsUser={
+    name: "rishu",
+    age: 18,
+    location: "azamgarh",
+    email: "rishu@gmail.com",
+    isLoggedIn: false,
+    lostLoginDays: ["Monday", "Saturday"]
+}
