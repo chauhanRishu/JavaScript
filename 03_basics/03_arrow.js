6ba1ad7cac3b2fe7ebc71ac9e27dd6815ca4b1ad
chauhanRishu <147832011+chauhanRishu@users.noreply.github.com>
2024-02-25 21:24:23 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,26 @@
+const user={
+    username: "rishu",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+//  user.username="raaz"
+// user.welcomeMessage()
+
+// console.log(this);
+
+/*
+function chai(){
+    let username="rishu"
+    console.log(this.username);
+}
+
+chai()
+*/
+
+cost 
