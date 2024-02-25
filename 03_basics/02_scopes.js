67fc9667e2f853b5c4ed26c6120a640d08f3228c
chauhanRishu <147832011+chauhanRishu@users.noreply.github.com>
2024-02-25 20:28:49 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,9 +4,55 @@ let a=300
 if(true){
     let a=10
     const b=20
-    console.log("INNER: ",a);
+    // console.log("INNER: ",a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(params) {
+    const username="rishu"
+
+    function two(){
+        const website= "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+
+if(true){
+    const username="rishu"
+    if(username=== "rishu"){
+        const website=" youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+//+++++++++++++++ interesting +++++++++++++++++++++
+
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+
+const addTwo=function(num){
+    return num + 2
+}
+
+
